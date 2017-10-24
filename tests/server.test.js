dc0163b2e4626f7962fe67f3122de0eb5ce5f824
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-10-24 13:23:31 +0300

    tests editing

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,30 +2,8 @@ var chai = require('chai');
 var request = require('request');
 var expect = chai.expect;
 
-//var sequelize = require('../database/main.js');
-
-// var beforEach = (cb) => cb();
-
-// describe('' , () => {
-//   //before each test we will launch the server ..
-//   before(() => {
-//     server = app.listen(3337, (err, data) => {
-//       if (!err) {
-//         console.log('server is listining');
-//       } else {
-//         console.log('server have error : ' , err);
-//       }
-//     })
-//   });
-//   //close the session after each test ...
-//   after(() => {
-//     request('http://localhost:3337/users/logout', function(error, res, body) {
-//       sequelize.close();
-//       server.close();
-//     });
-    
-//   });
-// })
+// var uri = 'https://thawing-garden-23809.herokuapp.com/';
+var uri = 'http://localhost:3336/';
 
 //examples : 
 // expect(foo).to.be.a('string');
@@ -37,22 +15,25 @@ var expect = chai.expect;
 describe('server' , () => {
   //check if the server listens(starts) correctly ...
   it('listens wothout crashing', (done) => {
-    request('https://thawing-garden-23809.herokuapp.com/', function(error, res, body) {
+    request(`${uri}`, function(error, res, body) {
       expect(body).to.exist;
       done();
     });
   });
-  describe('users/someEndPoint' , () => {
-    describe('/users', () => {
-      it('should response with status 302 if requested /users' , (done) => {
-        request('https://thawing-garden-23809.herokuapp.com/users', function(error, res, body) {
-          expect(res.statusCode).to.equal(302);
-          done();
+  describe('users end point' , () => {
+    describe('/users/', () => {
+      it('should response with status 302 if requested /users' , function (doneGetUsers) {
+        request(`${uri}users`, function(error, res, body) {
+          var code = res.statusCode;
+          console.log('status code for /users : ', code);
+          expect(code).to.equal(302);
+          doneGetUsers();
         })
       })
       it('should response with users array if requested /users' , (done) => {
-        request('https://thawing-garden-23809.herokuapp.com/users', function(error, res, body) {
+        request(`${uri}users`, function(error, res, body) {
           body = JSON.parse(body);
+          console.log(`recieved ${body.length} users`);
           expect(error).to.not.exist;
           expect(body).to.exist;
           expect(body , 'get/users', 'get/users').to.be.a('array');
@@ -69,70 +50,87 @@ describe('server' , () => {
         });
       })
     })
-
+    var user = {"username":"Haya","password":"1234","email":"haya@Users.thesis"};
     describe('/users/signup' , () => {
-      var user = {
-            "username":"Haya",
-            "password":"1234",
-            "email":"ashar@Users.thesis"
-          };
       var opts = {
-        "uri" : "https://thawing-garden-23809.herokuapp.com/users/signup",
-        "methode" : "POST",
+        "uri" : `${uri}users/signup`,
+        "method" : "POST",
         "json" : user
       }
-      request({
-        "uri" : "https://thawing-garden-23809.herokuapp.com/users/deleteuser",
-        "methode" : "POST",
-        "json" : {"username" : "Haya"}
-      }, (err, res, body) => {
-        //we delete the user to make sure our request will work no matter the times we call it ... 
-        //all "it" down here are inside the callback of the deleteuser request 
-        it('should response with status 201 if requested /users/signup with valid user info' , (done) => {
-          request(opts, (err, res, body) => {
-            expect(res.statusCode).to.equal(201);
-            done();
-          })
-        })
-        it('should store user data requested /users/signup with valid user info' , (done) => {
-          request('https://thawing-garden-23809.herokuapp.com/users', function(error, res, body) {
-            body = JSON.parse(body);
-            console.log(body)
-            var dbUser = body.reduce((acc, ele) => {
-              console.log(user.username ,ele.username)
-              if (user.username === ele.username) {
-                console.log(acc, ele);
-                acc = ele;
-              }
-              return acc;
-            }, undefined);
-            expect(dbUser.username).to.equal("Haya")
-            expect(dbUser.password).to.equal("1234")
-            expect(dbUser.email).to.equal("ashar@Users.thesis");
-            done();
-          });
-        })
-        xit('should response with status 400 if requested /users/signup with invalid user info' , (done) => {
-          done();
-        });
-        xit('shouldn\'t allow two simmilar usernames', (done) => {
-          done();
-        })
-      }) //end of request deleteuser callback 
-    }) //end of decribe signup
+      it('should response with status 201 if requested /users/signup with valid user info' , (doneOfSignupStatus) => {
+        request(opts, (err, res, body) => {
+          if (err) {
+            console.log(err.message , ' status : ' ,res.statusCode);
+          } else {
+            console.log('no errors , saved : ' , JSON.parse(body).saved)
+          }
+          expect(res.statusCode).to.equal(201);
+          doneOfSignupStatus();
+        })// request signup  ***********************
+      })//end it  signup status .........doneOfDeleteStatus...............
+      it('should store user data requested /users/signup with valid user info' , (doneOfStoreUser) => {
+        request(`${uri}users`, function(error, res, body) {
+          body = JSON.parse(body);
+          if (error) {
+            console.log('error signup : ' , error.message);
+          } else {
+            console.log(body);
+          }
+          var dbUser = body.reduce((acc, ele) => {
+            console.log(user.username ,ele.username)
+            if (user.username === ele.username) {
+              console.log(acc, ele);
+              acc = ele;
+            }
+            return acc;
+          }, undefined);
+          expect(dbUser).to.exist;
+          expect(dbUser.username).to.equal("Haya")
+          expect(dbUser.password).to.equal("1234")
+          expect(dbUser.email).to.equal("haya@Users.thesis");
+         
+          console.log('done of them all ..................')
+          doneOfStoreUser();
+        }) //end of get users "request" ***********************
+      }) //end it should store data .........doneOfStoreUser..........
+      it('should response with status 400 if requested /users/signup with two simmilar usernames' , (doneFailingSignUp) => {
+        request(opts, (err, res, body) => {
+          expect(res.statusCode).to.equal(400);
+          expect(body.saved).to.equal(false);
+          doneFailingSignUp();
+        })// request signup  ***********************
+      }) // end it
+    }) //end of decribe signup 
+
+    describe('/users/deleteuser', () => {
+      it ('should delete the user at request /deleteuser', (doneOfDelete) => {
+        request({
+          "uri" : `${uri}users/deleteuser`,
+          "method" : "POST",
+          "json" : {"username" : "Ashar"}
+        }, (error, response, resbody) => {
+          //we delete the user to make sure our request will work no matter the times we call it ... 
+          console.log('error : ', error);
+          if (!error) {
+            expect(response.statusCode).to.equal(202); 
+          }
+          doneOfDelete();
+        }) //end of request deleteuser callback  ***********************
+      }) //end of "it" delete user ..........doneOfDelete................
+    })
 
     describe('users/signin/' , () => {
-      xit('should response with user\'s info if requested /users/userinfo for a signed-in user' , (done) => {
+      it('should response with user\'s info if requested /users/userinfo for a signed-in user' , (done) => {
         done();
-      })
-      xit('should response with status 400 if requested /users/userinfo for a not signed-in user' , (done) => {
+      }) // end it
+      it('should response with status 400 if requested /users/userinfo for a not signed-in user' , (done) => {
         done();
-      })
-    })
+      }) // end it
+    }) // describe users/signin/
 
     describe('user/signout', () => {
-      xit('should')
-      xit('should response with 401 : not authrised if requested /user/info with a not-signed-in user')
-    })
-  })
-})
+      it('should')
+      it('should response with 401 : not authrised if requested /user/info with a not-signed-in user')
+    }) // describe user/signout
+  }) //describe users end point
+}) //describe server ....
